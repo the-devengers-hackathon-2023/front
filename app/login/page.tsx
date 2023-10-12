86c0abea6c11deb49bed9539ed1b1fdb3b0b4ee9
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2023-10-12 13:47:15 +0200

    width full login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-61.6px)] bg-white dark:bg-transparent rounded-lg">
+    <div className="w-full flex justify-center items-center h-[calc(100vh-61.6px)] bg-white dark:bg-transparent rounded-lg">
       <LoginForm />
     </div>
   );
