3836c8c750e4441b69129144cfa863ce65681f3c
sximenez <70152164+sximenez@users.noreply.github.com>
2023-10-12 12:57:01 +0200

    UPDATE responsive

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/LoginForm";
 
 export default function Login() {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-61.6px)] bg-white dark:bg-transparent p-14 rounded-lg">
+    <div className="flex justify-center items-center h-[calc(100vh-61.6px)] bg-white dark:bg-transparent rounded-lg">
       <LoginForm />
     </div>
   );
