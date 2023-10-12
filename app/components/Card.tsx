37eb62e78f9ba02d233e51c8f3b9eac6634fe8e1
sximenez <70152164+sximenez@users.noreply.github.com>
2023-10-12 16:30:15 +0200

    UPDATE festival page

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -18,29 +18,3 @@ export default function CardWithActionButton({ title }: { title: string }) {
     </Card>
   );
 }
-
-export function FestivalCard({
-  title,
-  region,
-  city,
-  category,
-}: {
-  title: string;
-  region: string;
-  city: string;
-  category: string;
-}) {
-  return (
-    <Card className="">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        <p>{title}</p>
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{region}</p>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{city}</p>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{category}</p>
-      <Button>
-        <p>Une erreur ?</p>
-      </Button>
-    </Card>
-  );
-}
