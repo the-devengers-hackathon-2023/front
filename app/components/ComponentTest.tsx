edb5b0a02df5339158ee62916236b3c54eec39ec
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2023-10-13 12:12:56 +0200

    Comment FilterButton

--- app/components/ComponentTest.tsx
+++ app/components/ComponentTest.tsx
@@ -13,8 +13,8 @@ export default function ComponentTest() {
             <div className="fixed flex flex-col items-center top-[61.6px] w-full bg-white dark:bg-black z-20 pt-4">
                 <Searchbar setSearchResults={setSearchResults}/>
             <div className="w-3/4 pb-4 border-b dark:border-white border-gray-900">
-                <FilterButton text={"Bonsoir"} />
-                <FilterButton text={"Merci"} />
+                {/* <FilterButton text={"Bonsoir"} />
+                <FilterButton text={"Merci"} /> */}
             </div>
             </div>
             <Suspense fallback={<Loading />}>
